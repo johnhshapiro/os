cf44ca30cbd5b48a0c49c9798e0da7103ebd5cce
John Shapiro <john@debian.virtual>
2019-06-06 14:42:09 -0600

    added declaration for pid

--- counter.c
+++ counter.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
+#include <sys/types.h>
 
 int main(char int_string[]) {
     int number = atoi(int_string);
-    for (int i = 1; i = number; i++);
-    
+    pid_t pid = getpid();
+    for (int i = 1; i <= number; i++){
+        printf("Process: ", number, " ", i);
+    }
     return number;
 }
