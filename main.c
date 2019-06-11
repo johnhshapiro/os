678cef9a18e3d0164c4a36976052bc4944ef884b
John Shapiro <john@debian.virtual>
2019-06-11 14:35:41 -0600

    changed if elif else to be accurate

--- main.c
+++ main.c
@@ -12,15 +12,17 @@ int main() {
     int parent_pid = getppid();
     int exitStatus;
 
-    if(pid < 0) {
+    if (pid < 0) {
         perror("fork"); 
         exit(-1);
     }
-    else if(pid == 0) {
-        printf("Child PID: %d\nParent PID: %d\n", child_pid, parent_pid);
-        execl("./counter", "counter", "5", (char *) NULL);
+    else if (pid > 0) {
+        //printf("this is the parent");
+        // WHY DOES THE ABOVE LINE STILL EXECUTE EVEN THOUGH IT'S NOT TRUE!?
     }
     else {
+        printf("Child PID: %d\nParent PID: %d\n", child_pid, parent_pid);
+        execl("./counter", "counter", "5", (char *) NULL);
     }
     waitpid(pid, &exitStatus, 0); 
 
