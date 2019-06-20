c3f376988b55b99c04923aba6187aa7ce28fbe00
johnhshapiro <john@debian.virtual>
2019-06-19 20:57:16 -0600

    added stuff

--- assignment_2/child.c
+++ assignment_2/child.c
@@ -1,14 +1,16 @@
 #include <sys/types.h>
-#include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
 #include <assert.h>
-#include <stdlib.h>
 
 int main() {
     assert(kill(getppid(), SIGUSR1) == 0);
     assert(kill(getppid(), SIGUSR2) == 0);
+    assert(kill(getppid(), SIGURG) == 0);
+    
+    assert(kill(getppid(), SIGURG) == 0);
+    assert(kill(getppid(), SIGURG) == 0);
     assert(kill(getppid(), SIGURG) == 0);
     return(0);
 }
