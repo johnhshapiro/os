386e4737d7896115b264cbcca86ea1a731e9fccb
johnhshapiro <john@debian.virtual>
2019-06-26 11:18:25 -0600

    added eye2eh, syscall, and child

---
+++ assignment_3/child.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    pid_t pid = getpid();
+    while (1) {
+        printf("awake in %d\n", pid);
+        sleep(1);
+    }
+}
